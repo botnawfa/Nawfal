7f2c5c1474a9c2304fcd678e1688dd2f599c489c
botnawfa <159430210+botnawfa@users.noreply.github.com>
2024-03-09 11:25:28 +0100

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,17 +2,17 @@ import yts from 'yt-search';
 import fs from 'fs';
 
 const handler = async (m, {conn, text}) => {
-  if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*';
+  if (!text) throw '⚠️ *ما الذي تريدني أن أبحث عنه في اليوتيوب؟?_*';
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
     switch (v.type) {
       case 'video': return `
 ° *_${v.title}_*
-↳ 🫐 *_Link :_* ${v.url}
-↳ 🕒 *_Duración :_* ${v.timestamp}
-↳ 📥 *_Subido :_* ${v.ago}
-↳ 👁 *_Vistas :_* ${v.views}`;
+↳ 🫐 *_الرابط :_* ${v.url}
+↳ 🕒 *_مدة :_* ${v.timestamp}
+↳ 📥 *_تم الرفع :_* ${v.ago}
+↳ 👁 *_الآراء :_* ${v.views}`;
     }
   }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
   conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
