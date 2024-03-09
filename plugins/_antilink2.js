40c98483f66ade00be97018ad99b4ce61dbbe9b5
botnawfa <159430210+botnawfa@users.noreply.github.com>
2024-03-09 11:32:54 +0100

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -21,13 +21,13 @@ export async function before(m, {conn, isAdmin, isBotAdmin, text}) {
       if (m.text.includes(linkThisGroup2)) return !0;
       if (m.text.includes(linkThisGroup3)) return !0;
     }
-    await this.sendMessage(m.chat, {text: `_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] El participante @user envió un enlace de un grupo de WhatsApp, por lo que será eliminado.*`, mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply('_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] Para que el anti-link funcione correctamente es necesario que el bot sea administrador del grupo.*');
+    await this.sendMessage(m.chat, {text: `_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] قام @user المشارك بإرسال رابط لمجموعة واتساب، لذلك سيتم حذفه.*`, mentions: [m.sender]}, {quoted: m});
+    if (!isBotAdmin) return m.reply('_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] تم تمكين Antilink، لكن المشارك الذي أرسل الارتباط هو مسؤول.*');
     if (isBotAdmin && bot.restrict) {
       await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
-    } else if (!bot.restrict) return m.reply('_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] Esta función está desactivada debido a que la función* _restrict_ *está habilitada.*');
+    } else if (!bot.restrict) return m.reply('_*< ANTI-LINK 2 />*_\n\n*[ ℹ️ ] تم تعطيل هذه الميزة بسبب تمكين الميزة* _restrict_ *.*');
   }
   return !0;
 }
