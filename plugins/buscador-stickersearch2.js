6fef900adfe4b1ced84d9939a49f7a28015e6c83
botnawfa <159430210+botnawfa@users.noreply.github.com>
2024-03-11 09:50:01 +0000

    Update buscador-stickersearch2.js

--- plugins/buscador-stickersearch2.js
+++ plugins/buscador-stickersearch2.js
@@ -3,17 +3,17 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {text, usedPrefix, command, conn}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*`;
+  if (!text) throw `*[❗] أدخل اسم الحزمة التي تريد البحث عنها*`;
   try {
     const res2 = await googleImage(text);
     const sfoto = res2.getRandom();
     const json = await fetch(`https://api.lolhuman.xyz/api/stickerwa?apikey=${lolkeysapi}&query=${text}`);
     const jsons = await json.json();
     const {stickers} = jsons.result[0];
-    const res = jsons.result.map((v, index) => `🌅 • Resultado: ${1 + index}\n*🥗 • Nombre:* ${v.title}\n*🐢 • Autor:* ${v.author}\n*🍂 • Url:* ${v.url}`).join`\n\n───\n\n`;
+    const res = jsons.result.map((v, index) => `🌅 • نتيجة: ${1 + index}\n*🥗 • إسم:* ${v.title}\n*🐢 • مؤلف:* ${v.author}\n*🍂 • Url:* ${v.url}`).join`\n\n───\n\n`;
     await conn.sendFile(m.chat, sfoto, 'error.jpg', res, m);
   } catch {
-    await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*');
+    await m.reply('*[❗] خطأ، يرجى المحاولة مرة أخرى*');
   }
 };
 handler.command = ['stickersearch2', 'searchsticker2', 'stickerssearch2', 'searchstickers2'];
