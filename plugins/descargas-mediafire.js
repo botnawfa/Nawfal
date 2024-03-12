ec864a036ca05ab3065dc2ce13dac06807ec66b7
botnawfa <159430210+botnawfa@users.noreply.github.com>
2024-03-12 11:52:39 +0000

    Update descargas-mediafire.js

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -4,29 +4,29 @@ import cheerio from 'cheerio';
 import {mediafiredl} from '@bochilteam/scraper';
 
 const handler = async (m, {conn, args, usedPrefix, command}) => {
-  if (!args[0]) throw `_*< DESCARGAS - MEDIAFIRE />*_\n\n*[ ℹ️ ] Ingrese un enlace de MediaFire.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE_`;
+  if (!args[0]) throw `_*< التحميلات - MEDIAFIRE />*_\n\n*[ ℹ️ ] أدخل رابط ميديا ​​فاير.*\n\n*[ 💡 ] مثال:* _${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE_`;
   try {
     const resEX = await mediafiredl(args[0]);
-    const captionES = `_*< DESCARGAS - MEDIAFIRE />*_\n
-▢ *Nombre:* ${resEX.filename}
-▢ *Tamaño:* ${resEX.filesizeH}
-▢ *Extensión:* ${resEX.ext}\n\n
-*[ ℹ️ ] Se está enviando el archivo. espere...*`.trim();
+    const captionES = `_*< التحميلات - MEDIAFIRE />*_\n
+▢ *إسم:* ${resEX.filename}
+▢ *مقاس:* ${resEX.filesizeH}
+▢ *امتداد:* ${resEX.ext}\n\n
+*[ ℹ️ ] يتم الآن إرسال الملف.  انتظر...*`.trim();
     m.reply(captionES);
     await conn.sendFile(m.chat, resEX.url, resEX.filename, '', m, null, {mimetype: resEX.ext, asDocument: true});
   } catch {
     try {
       const res = await mediafireDl(args[0]);
       const {name, size, date, mime, link} = res;
-      const caption = `_*< DESCARGAS - MEDIAFIRE />*_\n
-▢ *Nombre:* ${name}
-▢ *Tamaño:* ${size}
-▢ *Extensión:* ${mime}\n\n
-*[ ℹ️ ] Se está enviando el archivo. espere...*`.trim();
+      const caption = `_*< التحميلات - MEDIAFIRE />*_\n
+▢ *إسم:* ${name}
+▢ *مقاس:* ${size}
+▢ *امتداد:* ${mime}\n\n
+*[ ℹ️ ] يتم الآن إرسال الملف.  انتظر...*`.trim();
       await m.reply(caption);
       await conn.sendFile(m.chat, link, name, '', m, null, {mimetype: mime, asDocument: true});
     } catch {
-      await m.reply('_*< DESCARGAS - MEDIAFIRE />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*');
+      await m.reply('_*< التحميلات - MEDIAFIRE />*_\n\n*[ ℹ️ ] حدث خطأ.  الرجاء معاودة المحاولة في وقت لاحق.*');
     }
   }
 };
