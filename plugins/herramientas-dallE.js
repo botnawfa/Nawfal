1541a2222bfa18294fd2badcc467e077dcb0b0f6
botnawfa <159430210+botnawfa@users.noreply.github.com>
2024-03-14 23:06:41 +0000

    Update herramientas-dallE.js

--- plugins/herramientas-dallE.js
+++ plugins/herramientas-dallE.js
@@ -1,30 +1,30 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙲𝚁𝙴𝙰𝚁 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚈 𝙰𝚂𝙸 𝚄𝚂𝙰𝚁 𝙻𝙰 𝙵𝚄𝙽𝙲𝙸𝙾𝙽 𝙳𝙴 𝙳𝙰𝙻𝙻-𝙴*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾𝚂 𝙳𝙴 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽𝙴𝚂*\n*◉ ${usedPrefix + command} gatitos llorando*\n*◉ ${usedPrefix + command} hatsune miku beso*`;
-    await conn.sendMessage(m.chat, {text: '*[❗] Realizando imagen, aguarde un momento.*'}, {quoted: m});
+  if (!text) throw `*[❗] أدخل نصًا لإنشاء صورة، ومن ثم استخدم الدالة 𝙳𝙰𝙻𝙻-𝙴*\n\n*—◉ أمثلة على الطلبات*\n*◉ ${usedPrefix + command} القطط البكاء*\n*◉ ${usedPrefix + command} السعر الأولي ميكو بيسو*`;
+    await conn.sendMessage(m.chat, {text: '*[❗] 💖 جاري صنع الصورة إنتظر لحضة 🌹.*'}, {quoted: m});
   try {
     const tiores1 = await fetch(`https://vihangayt.me/tools/imagine?q=${text}`);
     const json1 = await tiores1.json();
     await conn.sendMessage(m.chat, {image: {url: json1.data}}, {quoted: m});
   } catch {  
-      console.log('[❗] Error en la api numero 1 de dall-e.');  
+      console.log('[❗] خطأ في API رقم 1 dall-e.');  
   try {
     const tiores2 = await conn.getFile(`https://vihangayt.me/tools/midjourney?q=${text}`);
     await conn.sendMessage(m.chat, {image: {url: tiores2.data}}, {quoted: m});
   } catch {
-      console.log('[❗] Error en la api numero 2 de dall-e.');
+      console.log('[❗] خطأ في API رقم 2 dall-e.');
   try {
     const tiores3 = await fetch(`https://vihangayt.me/tools/lexicaart?q=${text}`);
     const json3 = await tiores3.json();
     await conn.sendMessage(m.chat, {image: {url: json3.data[0].images[0].url}}, {quoted: m});
   } catch {
-      console.log('[❗] Error en la api numero 3 de dall-e.');
+      console.log('[❗] خطأ في API رقم 3 dall-e.');
   try {
     const tiores4 = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`);
     await conn.sendMessage(m.chat, {image: {url: tiores4.data}}, {quoted: m});
   } catch {
-    console.log('[❗] Error, ninguna api funcional.');
-    throw `*[❗] Error, no se obtuvierón resultados.*`;
+    console.log('[❗] خطأ، لا توجد واجهة برمجة تطبيقات تعمل.');
+    throw `*[❗] خطأ، لم يتم الحصول على أي نتائج😞.*`;
   }}
  }}
 };
