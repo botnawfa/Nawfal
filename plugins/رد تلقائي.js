22f267a2865f976d4cdd7fcb171fb20ed0a695d5
botnawfa <159430210+botnawfa@users.noreply.github.com>
2024-03-14 15:54:10 +0000

    Create رد تلقائي.js

---
+++ plugins/رد تلقائي.js
@@ -0,0 +1,110 @@
+let handler = m => m; 
+ handler.all = async function (m) { 
+
+   let chat = global.db.data.chats[m.chat]; 
+   let responses; 
+   if (/^هلا$/i.test(m.text)) { 
+     responses = [ 
+ '*هلا بيك يعمري 😩❤‍🔥*'  
+     ]; 
+} else if (/^السلام عليكم|سلام عليكم ورحمه الله وبركاته|سلام عليكم|السلام عليكم ورحمه الله وبركاته$/i.test(m.text)) { 
+     responses = [ 
+       '*♥🐥وعليكم السلام*',  
+     ]; 
+   }else if (/^نوفل|نوفل$/i.test(m.text)) { 
+     responses = [ 
+'*بوت نوفل تحت امرك*'
+     ]; 
+ }else if (/^نوفل من عمك|نوفل عمك$/i.test(m.text)) { 
+     responses = [ 
+'*نوفل اككيد😩❤‍🔥*'
+     ]; 
+   }else if (/^نوفل هل انت مرتبط$/i.test(m.text)) { 
+     responses = [ 
+'*لا ولا أريد الارتباط🏌🏻‍♂💔*'
+   ]; 
+   }else if (/^نوفل تحبني؟|بوت تحبني$/i.test(m.text)) { 
+     responses = [ 
+'*اموت فيك 🌚💔*',
+'*اكرهك🙂💔*',
+'*احبك نص حب 🙃💔*',
+]; 
+   }else if (/^نوفل تكرهني؟$/i.test(m.text)) { 
+     responses = [ 
+'*ماعاش من يكرهكك حبي 🙁*',
+'*لا بس لا تتعب نفسك لحبك🫥*',
+'*ااي اكرهك🙄*',   ]; 
+     
+     }else if (/^هاي|هالو$/i.test(m.text)) { 
+     responses = [ 
+       '*هالو🌚♥*',  
+
+     ]; 
+}else if (/^بحبك/i.test(m.text)) { 
+     responses = [ 
+       '*بحبك اكتر🌚❤*',  
+
+     ]; 
+   }else if (/^نوفل من وين انت؟$/i.test(m.text)) { 
+     responses = [ 
+'المغرب💕'
+     ]; 
+   } else if (/^احبك$/i.test(m.text)) { 
+     responses = [ 
+'*مـي تو 🙂🫀*'
+     ]; 
+     }else if (/^كيف حالك |عامل اي|عامل اية$/i.test(m.text)) { 
+     responses = [ 
+       'الحمدالله',  
+
+     ];
+     }else if (/^تحبني$/i.test(m.text)) { 
+     responses = [ 
+       '🌚♥اكيد',  
+
+     ];
+     }else if (/^هاي$/i.test(m.text)) { 
+     responses = [ 
+       'هاي',  
+
+     ];
+     }else if (/^❤|♥$/i.test(m.text)) { 
+     responses = [ 
+       '*اعشقق♥*',  
+
+     ];
+     }else if (/^اهلا$/i.test(m.text)) { 
+     responses = [ 
+       '*اهلا♥*',  
+
+     ]; 
+     }else if (/^مساء|مساء$/i.test(m.text)) { 
+     responses = [ 
+       'مساء الخير',  
+
+     ];
+     }else if (/^صباح|صباح$/ .test(m.text)) { 
+     responses = [ 
+       '*صباح الورد♥*',  
+     ];
+       }else if (/^اوامر$/i.test(m.text)) { 
+     responses = [ 
+       '*لا تنسى ال .*',  
+     ];
+            }else if (/^nawfal$/i.test(m.text)) { 
+     responses = [ 
+       '*تحت امرك حبي*',  
+     ];
+            }else if (/^مرحبا$/i.test(m.text)) { 
+     responses = [ 
+       '*مرحبا♥*',  
+     ];
+   }
+   if (responses) { 
+     let randomIndex = Math.floor(Math.random() * responses.length); 
+     conn.reply(m.chat, responses[randomIndex], m); 
+   } 
+   return !0 
+ }; 
+
+ export default handler;
